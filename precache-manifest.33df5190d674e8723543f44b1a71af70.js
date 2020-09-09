ff3bcaca4146247e8be3138080514a3144246191
Alexandr Moroz <afmoroz@gmail.com>
2020-09-09 21:41:38 +0300

    Updates

---
+++ precache-manifest.33df5190d674e8723543f44b1a71af70.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a080b30914695dca1357851083d74a4",
+    "url": "/react-minimal-site-one/index.html"
+  },
+  {
+    "revision": "a2930979cf1350218fd4",
+    "url": "/react-minimal-site-one/static/css/main.7e49c147.chunk.css"
+  },
+  {
+    "revision": "11fb4aa50ea03b42ab89",
+    "url": "/react-minimal-site-one/static/js/2.9e980678.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-minimal-site-one/static/js/2.9e980678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2930979cf1350218fd4",
+    "url": "/react-minimal-site-one/static/js/main.af1be2e2.chunk.js"
+  },
+  {
+    "revision": "1fea2536886f48317bfa",
+    "url": "/react-minimal-site-one/static/js/runtime-main.9acc6fe8.js"
+  },
+  {
+    "revision": "882a57264d9041f8c52b12ff804e0f27",
+    "url": "/react-minimal-site-one/static/media/bricks.882a5726.jpg"
+  },
+  {
+    "revision": "41ae3b1f9400c1c3cd13cf038fa9bfb2",
+    "url": "/react-minimal-site-one/static/media/close.41ae3b1f.svg"
+  },
+  {
+    "revision": "0ebcd8ae0836d83881252473f59e2dae",
+    "url": "/react-minimal-site-one/static/media/fi.0ebcd8ae.jpg"
+  },
+  {
+    "revision": "422c32f06295cd9f09232c933e982e17",
+    "url": "/react-minimal-site-one/static/media/galerie.422c32f0.jpg"
+  },
+  {
+    "revision": "a4e6bfc7a854c6ebc18a33b694e9dc37",
+    "url": "/react-minimal-site-one/static/media/search.a4e6bfc7.svg"
+  }
+]);
